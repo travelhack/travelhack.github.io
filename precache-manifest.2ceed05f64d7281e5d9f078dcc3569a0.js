a51d9bace714a97221f63679e8b614e62e38ed8a
Will Taylor <witaylor@expedia.com>
2019-11-15 10:01:14 +0000

    Updates

---
+++ precache-manifest.2ceed05f64d7281e5d9f078dcc3569a0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94fae55072428db87fca292c3a017a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82bf7e9ff6578ee17156",
+    "url": "/static/css/main.1485f52e.chunk.css"
+  },
+  {
+    "revision": "74fc8fc8ab89d88a9b55",
+    "url": "/static/js/2.c91572d7.chunk.js"
+  },
+  {
+    "revision": "82bf7e9ff6578ee17156",
+    "url": "/static/js/main.2c6e047a.chunk.js"
+  },
+  {
+    "revision": "d7457405a4cabe07142a",
+    "url": "/static/js/runtime~main.5f6149fd.js"
+  },
+  {
+    "revision": "a173db19bc7aedff4f76efcec5dccd0c",
+    "url": "/static/media/EG-logo.a173db19.png"
+  },
+  {
+    "revision": "43b455325231a88dccfbef7a389fad79",
+    "url": "/static/media/ExpediaGroupDisplay-Bold.43b45532.woff2"
+  },
+  {
+    "revision": "61e24f1d8aa531e475e2acabee176ce5",
+    "url": "/static/media/ExpediaGroupDisplay-Bold.61e24f1d.woff"
+  },
+  {
+    "revision": "75773973355590e17e62291dde1c90ae",
+    "url": "/static/media/fire-evac.75773973.png"
+  },
+  {
+    "revision": "7fbc0826790625327bdbe2ba88145372",
+    "url": "/static/media/logo_Hcom.7fbc0826.png"
+  },
+  {
+    "revision": "427127cdedfe85cf46a1919a8e85a81b",
+    "url": "/static/media/logo_bex.427127cd.png"
+  },
+  {
+    "revision": "edd14f473a96a08a817145832068f86f",
+    "url": "/static/media/logo_vrbo.edd14f47.png"
+  },
+  {
+    "revision": "578c55aa8eab971be3b2cac7cde5781a",
+    "url": "/static/media/pageNotFound.578c55aa.gif"
+  },
+  {
+    "revision": "7b2b2e413688f482eb0c1693ac2abe90",
+    "url": "/static/media/travelhack_logo.7b2b2e41.png"
+  }
+]);
